dcdb28462575db423adc9e9e80151a48582b37ab
Davi <1747735007@qq.com>
2019-09-12 13:05:46 +0800

    启动成功打印NICE

--- src/main/java/com/xw/SpringApplicationStarter.java
+++ src/main/java/com/xw/SpringApplicationStarter.java
@@ -1,14 +1,29 @@
 package com.xw;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.xw.user.mapper"})
+@Slf4j
 public class SpringApplicationStarter {
     public static void main(String[] args) {
         SpringApplication.run(SpringApplicationStarter.class);
+
+        log.info("                                                                                                \n\n\n\n \n" +
+                "               NNN        NN            II              CCC C CC                   EEEEEEEEEEEEE           \n"+
+                "             NN NN       NN            II             CCC       CC                EE                       \n"+
+                "            NN  NN      NN            II             CC                          EE                        \n"+
+                "           NN   NN     NN            II             CC                          EE                         \n"+
+                "          NN    NN    NN            II             CC                          EEEEEEEEEEEEEE              \n"+
+                "         NN     NN   NN            II             CC                          EE                           \n"+
+                "        NN      NN  NN            II              CC          CC             EE                            \n"+
+                "       NN       NN NN            II               CC       CCC             EE                             \n"+
+                "      NN        NNN             II                  CC CC CC               EEEEEEEEEEEEEEE                 \n");
     }
+
+
 }
